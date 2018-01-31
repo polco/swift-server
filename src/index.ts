49034587947c67ec138e32ae92e0d42c1a0bdc49
Paul Colleoni <paul.colleoni@gmail.com>
2018-02-01 00:03:23 +0900

    use self-signed cert for development

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,33 @@
+import * as express from 'express';
+import * as fs from 'fs';
 import * as logger from 'heroku-logger';
+import * as http from 'http';
+import * as https from 'https';
 import * as SocketIO from 'socket.io';
 
-const io = SocketIO();
+const app = express();
+const server = process.env.NODE_ENV === 'developement'
+	? https.createServer({
+		ca: fs.readFileSync('./ssl/cert.pem'),
+		cert: fs.readFileSync('./ssl/cert.pem'),
+		key: fs.readFileSync('./ssl/key.pem'),
+		rejectUnauthorized: false,
+		requestCert: false,
+	}, app)
+	: http.createServer(app);
 
 const port = process.env.PORT || 3434;
-io.listen(port);
+server.listen(port);
+
+const io = SocketIO();
+io.listen(server);
 logger.info(`Starting socket.io on port ${port}.`);
 
-const sockets: {[id: string]: SocketIO.Socket} = {};
+const sockets: { [id: string]: SocketIO.Socket } = {};
 
 io.on('connection', (socket) => {
 	logger.info(`Client connected: ${socket.id}.`);
+	sockets[socket.id] = socket;
 
 	socket.on('data', (msg) => {
 		logger.info(`received messaged`, msg);
