addc85113cebf2c1053b71ac74a595117cbede43
Paul Colleoni <paul.colleoni@gmail.com>
2018-01-31 12:59:16 +0900

    save connected socket into sessions

--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,26 @@ const port = process.env.PORT || 3434;
 io.listen(port);
 logger.info(`Starting socket.io on port ${port}.`);
 
+const sockets: {[id: string]: SocketIO.Socket} = {};
+
 io.on('connection', (socket) => {
 	logger.info(`Client connected: ${socket.id}.`);
 
 	socket.on('data', (msg) => {
 		logger.info(`received messaged`, msg);
-		socket.emit('data', msg);
+		const remoteSocket = sockets[msg.remoteId];
+		if (!remoteSocket) {
+			return logger.error('unknown remoteId', msg.remoteId);
+		}
+
+		remoteSocket.emit('data', {
+			data: msg.data,
+			fromId: socket.id
+		});
+	});
+
+	socket.on('disconnect', () => {
+		logger.info(`Client disconnected: ${socket.id}.`);
+		delete sockets[socket.id];
 	});
 });
